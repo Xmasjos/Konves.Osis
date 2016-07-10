a98395e9cbc97e42ed1e843e3cf28987063ab510
skonves <stephen.konves@gmail.com>
2016-07-10 16:38:18 -0700

    add rudimentary tests

---
+++ tests/Konves.Osis.UnitTests/OsisSerializerTestFixture.cs
@@ -0,0 +1,41 @@
+﻿using Konves.Osis.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+
+namespace Konves.Osis.UnitTests
+{
+	[TestClass]
+	public class OsisSerializerTestFixture
+	{
+		[TestMethod]
+		public void Test()
+		{
+			// Arrange
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			string resourceName = "Konves.Osis.UnitTests.Data.eng-web_osis.xml";
+			osisCT document = null;
+			string outputString = null;
+
+			// Act
+			using (Stream input = assembly.GetManifestResourceStream(resourceName))
+			using (Stream output = new MemoryStream())
+			{
+				document = OsisSerializer.Deserialize(input);
+				OsisSerializer.Serialize(output, document);
+
+				output.Flush();
+				output.Position = 0;
+
+				using (StreamReader reader = new StreamReader(output))
+				{
+					outputString = reader.ReadToEnd();
+				}
+			}
+
+			// Assert
+			Assert.IsNotNull(document);
+			Assert.IsNotNull(outputString);
+		}
+	}
+}
