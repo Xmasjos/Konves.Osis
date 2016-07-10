3594bfab7e426aac9967df9010077929a005421e
skonves <stephen.konves@gmail.com>
2016-07-09 18:30:30 -0700

    inital commit

---
+++ src/Konves.Osis/OsisSerializer.cs
@@ -0,0 +1,20 @@
+﻿using Konves.Osis.ObjectModel;
+using System.IO;
+
+namespace Konves.Osis
+{
+	public class OsisSerializer
+	{
+		public static osisCT Deserialize(Stream stream)
+		{
+			var s = new System.Xml.Serialization.XmlSerializer(typeof(osisCT));
+			return (osisCT)s.Deserialize(stream);
+		}
+
+		public static void Serialize(Stream stream, osisCT document)
+		{
+			var s = new System.Xml.Serialization.XmlSerializer(typeof(osisCT));
+			s.Serialize(stream, document);
+		}
+	}
+}
---
+++ src/Konves.Osis/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+[assembly: AssemblyTitle("Konves.Osis")]
+[assembly: AssemblyDescription("OSIS Document serialization")]
+[assembly: AssemblyProduct("Konves.Osis")]
+[assembly: AssemblyCopyright("Copyright © Steve Konves 2016")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
