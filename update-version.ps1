c2677c42bdeb72b2387fb99edd8be49f0b9ad585
skonves <stephen.konves@gmail.com>
2016-07-10 20:37:27 -0700

    code cleanup

--- update-version.ps1
+++
@@ -1,4 +0,0 @@
-param([String]$file="x", [String]$version="asdf")
-(Get-Content "$($file)") `
-    -replace '<version>[^<]*?</version>', ('<version>{0}</version>' -f $version) |
-  Out-File "$($file)"
